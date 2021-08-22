4d3ec13e5b8f95545eda442aacc38933324ae80a
dumidu1998 <dumiduraj@gmail.com>
2021-08-22 16:54:30 +0530

    main method implemented

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Car Racers Sri Lanka!!!");
+        System.out.println("Lets register some cars first!\n");
+
+        System.out.println("Enter the unique number of a car: ");
+        String uniqueid = scanner.nextLine();
+        System.out.println("Enter the brand of the car: ");
+        String brand = scanner.nextLine();
+        System.out.println("Enter the sponsor of the car: ");
+        String sponsor = scanner.nextLine();
+        System.out.println("Enter the driver name of the car: ");
+        String driver = scanner.nextLine();
+
+        System.out.println(uniqueid + " " + brand + " " + sponsor + " " + driver);
+
     }
+
 }

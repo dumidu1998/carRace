caa2a9863a9ea599ca57ede4e945ea54e5f56bc7
dumidu1998 <dumiduraj@gmail.com>
2021-08-22 19:26:57 +0530

    changed

--- Main.java
+++ Main.java
@@ -12,7 +12,8 @@ public class Main {
         System.out.println("Lets register some cars first!\n");
 
         while (true) {
-            System.out.println("Enter Operation: \n[1] - Register new Car\n[2] - View All Cars\n");
+            System.out.println(
+                    "Enter Operation: \n[1] - Register new Car\n[2] - View All Cars\n[3] - Search by car Id\n[4] - Search by driver Name\n[5] - Delete a car\n");
             int operation = scanner.nextInt();
             switch (operation) {
                 case 1:
@@ -21,12 +22,25 @@ public class Main {
                 case 2:
                     ViewAllCars();
                     break;
+                case 3:
+                    ViewAllCars();
+                    break;
+                case 4:
+                    ViewAllCars();
+                    break;
+                case 5:
+                    ViewAllCars();
+                    break;
             }
         }
 
     }
 
     public static Car RegisterCar() {
+        if (carcount == 5) {
+            System.out.println("Sorry, we can't register more cars.\n");
+            return null;
+        }
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the unique number of a car: ");
         String uniqueid = scanner.nextLine();
@@ -45,6 +59,18 @@ public class Main {
     public static void ViewAllCars() {
         for (int i = 0; i < carcount; i++) {
             cararray[i].printCar();
+        }
+    }
+
+    public static void DeleteCar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the unique number of a car to be deleted: ");
+        String uniqueid = scanner.nextLine();
+        for (int i = 0; i < carcount; i++) {
+            if (cararray[i].getUniqueId().equals(uniqueid)) {
+                cararray[i] = null;
+                carcount--;
+            }
         }
     }
 
